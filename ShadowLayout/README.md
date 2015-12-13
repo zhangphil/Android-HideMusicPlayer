44bc8c5b6b6ab0ab5080720f48e7420b1e30d5d8
zhangphil <zhang.fei@msn.com>
2015-12-13 23:09:43 +0800

    zhangphil first commit

---
+++ ShadowLayout/README.md
@@ -0,0 +1,3 @@
+# ShadowLayout-for-eclipse
+original：<a href="https://github.com/ShogoMizumoto/ZDepthShadow" target="_blank">https://github.com/ShogoMizumoto/ZDepthShadow</a>  
+
---
+++ ShadowLayout/gen/app/mosn/zdepthshadowlayout/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package app.mosn.zdepthshadowlayout;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
